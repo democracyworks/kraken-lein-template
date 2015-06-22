dd12756e15dd180f491ae4ae4f8c1d536886d8d4
Wes Morgan <cap10morgan@gmail.com>
2015-06-22 16:09:09 -0600

    [WM] bump version to 0.2.2
    ...and add a description in project.clj.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject kraken/lein-template "0.2.1"
-  :description "FIXME: write description"
+(defproject kraken/lein-template "0.2.2"
+  :description "Leiningen template for kraken components"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
