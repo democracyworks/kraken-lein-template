6133a277ad0b436bd8bf3016887a183b2cd31d5e
Eric Normand <ericwnormand@gmail.com>
2015-07-03 14:10:21 -0500

    [EN] Bump kehaar version and include schema.
    Also bump Clojure to 1.7.0.

--- resources/leiningen/new/kraken/project.clj
+++ resources/leiningen/new/kraken/project.clj
@@ -3,14 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [turbovote.resource-config "0.1.4"]
                  [com.novemberain/langohr "3.2.0"]
                  [democracyworks/datomic-toolbox "1.0.0" :exclusions [com.datomic/datomic-pro]]
                  [com.datomic/datomic-pro "0.9.5153" :exclusions [joda-time]]
                  [org.immutant/core "2.0.0"]
-                 [democracyworks/kehaar "0.2.1"]]
+                 [democracyworks/kehaar "0.3.1"]
+                 [prismatic/schema "0.4.3"]]
   :plugins [[lein-immutant "2.0.0"]]
   :main ^:skip-aot {{name}}.core
   :target-path "target/%s"
