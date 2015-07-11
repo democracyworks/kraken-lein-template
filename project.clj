ebfa3e6a8a913bd2fbce57c19e0a2e1533142147
Wes Morgan <cap10morgan@gmail.com>
2015-07-11 12:48:04 -0400

    [WM] bump version to 0.3.0-SNAPSHOT
    Apparently `lein release` always starts by dropping the SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject kraken-works/lein-template "0.2.3-SNAPSHOT"
+(defproject kraken-works/lein-template "0.3.0-SNAPSHOT"
   :description "Leiningen template for kraken-works components"
   :url "https://github.com/democracyworks/kraken-works-lein-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars]]
   :eval-in-leiningen true)
