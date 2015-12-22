f5d1e5e678c64af194ee164cd7944082b537d4b0
Wes Morgan <cap10morgan@gmail.com>
2015-12-22 10:49:01 -0500

    [WM] delete unused requires from queue ns template

--- resources/leiningen/new/kraken_works/queue.clj
+++ resources/leiningen/new/kraken_works/queue.clj
@@ -1,9 +1,6 @@
 (ns {{name}}.queue
   (:require [clojure.tools.logging :as log]
             [langohr.core :as rmq]
-            [langohr.channel :as lch]
-            [langohr.exchange :as le]
-            [langohr.queue :as lq]
             [kehaar.rabbitmq]
             [kehaar.wire-up :as wire-up]
             [{{name}}.channels :as channels]
