56e71c939b04c8861435015b0da14a49da694002
Wes Morgan <cap10morgan@gmail.com>
2015-12-22 16:18:35 -0500

    [WM] fix require indentation in core ns

--- resources/leiningen/new/kraken_works/core.clj
+++ resources/leiningen/new/kraken_works/core.clj
@@ -1,10 +1,10 @@
 (ns {{name}}.core
-    (:require [{{name}}.channels :as channels]
-              [{{name}}.queue :as queue]
-              [turbovote.resource-config :refer [config]]
-              [datomic-toolbox.core :as db]
-              [clojure.tools.logging :as log]
-              [immutant.util :as immutant]))
+  (:require [{{name}}.channels :as channels]
+            [{{name}}.queue :as queue]
+            [turbovote.resource-config :refer [config]]
+            [datomic-toolbox.core :as db]
+            [clojure.tools.logging :as log]
+            [immutant.util :as immutant]))
 
 (defn -main [& args]
   (cond (config [:datomic :initialize]) (db/initialize (config [:datomic]))
