a39b361903e02a1e01252a17628c86a6787e92b3
Wes Morgan <cap10morgan@gmail.com>
2015-12-23 13:01:10 -0500

    Version 0.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kraken-works/lein-template "0.4.0"
+(defproject kraken-works/lein-template "0.4.1-SNAPSHOT"
   :description "Leiningen template for kraken-works components"
   :url "https://github.com/democracyworks/kraken-works-lein-template"
   :license {:name "Eclipse Public License"
