9a7aef10d70bcaa9060b06fffb95b55fb5b29325
Wes Morgan <cap10morgan@gmail.com>
2016-01-13 11:36:15 -0700

    [WM] update generated deps to latest versions

--- resources/leiningen/new/kraken_works/project.clj
+++ resources/leiningen/new/kraken_works/project.clj
@@ -6,13 +6,13 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [turbovote.resource-config "0.2.0"]
-                 [com.novemberain/langohr "3.4.2"]
+                 [com.novemberain/langohr "3.5.0"]
                  [democracyworks/datomic-toolbox "2.0.0" :exclusions [com.datomic/datomic-pro]]
                  [prismatic/schema "1.0.4"]
                  [com.datomic/datomic-pro "0.9.5327" :exclusions [org.slf4j/slf4j-nop
                                                                   org.slf4j/slf4j-log4j12]]
                  [ch.qos.logback/logback-classic "1.1.3"]
-                 [org.immutant/core "2.1.1"]
+                 [org.immutant/core "2.1.2"]
                  [democracyworks/kehaar "0.5.0"]]
   :plugins [[lein-immutant "2.1.0"]]
   :main ^:skip-aot {{name}}.core
