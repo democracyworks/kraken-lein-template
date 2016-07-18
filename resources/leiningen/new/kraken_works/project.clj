1a8a203e5598daa39a57b2d8d350d6da65270b8e
Wes Morgan <wesmorgan@icloud.com>
2016-07-18 10:36:14 -0600

    Update deps in generated project.clj
    These all seem to work OK in actual projects based on this template.

--- resources/leiningen/new/kraken_works/project.clj
+++ resources/leiningen/new/kraken_works/project.clj
@@ -3,16 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [turbovote.resource-config "0.2.0"]
-                 [com.novemberain/langohr "3.5.0"]
+                 [com.novemberain/langohr "3.6.1"]
                  [democracyworks/datomic-toolbox "2.0.1" :exclusions [com.datomic/datomic-pro]]
-                 [prismatic/schema "1.0.4"]
+                 [prismatic/schema "1.1.2"]
                  [com.datomic/datomic-pro "0.9.5327" :exclusions [org.slf4j/slf4j-nop
                                                                   org.slf4j/slf4j-log4j12]]
-                 [ch.qos.logback/logback-classic "1.1.3"]
-                 [org.immutant/core "2.1.2"]
+                 [ch.qos.logback/logback-classic "1.1.7"]
+                 [org.immutant/core "2.1.5"]
                  [democracyworks/kehaar "0.5.0"]]
   :plugins [[lein-immutant "2.1.0"]]
   :main ^:skip-aot {{name}}.core
