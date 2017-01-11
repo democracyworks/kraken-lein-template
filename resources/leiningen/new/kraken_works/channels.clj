767964d8e4a4c5e3f433fb6bb8ac5922a3be64b0
Christopher Shea <cmshea@gmail.com>
2017-01-11 15:15:44 -0500

    Explanatory comment for channels.clj

--- resources/leiningen/new/kraken_works/channels.clj
+++ resources/leiningen/new/kraken_works/channels.clj
@@ -1,6 +1,13 @@
 (ns {{name}}.channels
   (:require [clojure.core.async :as async]))
 
+;;; This namespace is for core.async channels used by kehaar. Channels
+;;; only need to be created for ougoing events and external
+;;; services. If neither are required, this namespace can be
+;;; removed. Add any created channels to the vector of channels
+;;; defined in `close-all!`.
+
 (defn close-all! []
-  (doseq [c []]
-    (async/close! c)))
+  (let [channels []]
+    (doseq [c channels]
+      (async/close! c))))
