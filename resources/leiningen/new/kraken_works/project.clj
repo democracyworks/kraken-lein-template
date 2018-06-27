d3472d6e8b31bb0bb03a4dfa1c44347221b812c5
Mike Richards <mrichards@democracy.works>
2018-06-27 10:22:57 -0400

    Update the rest of the dependencies

--- resources/leiningen/new/kraken_works/project.clj
+++ resources/leiningen/new/kraken_works/project.clj
@@ -4,19 +4,19 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.logging "0.4.0"]
+                 [org.clojure/tools.logging "0.4.1"]
                  [turbovote.resource-config "0.2.1"]
-                 [democracyworks/datomic-toolbox "2.0.4"
+                 [democracyworks/datomic-toolbox "2.0.5"
                   :exclusions [com.datomic/datomic-pro]]
-                 [com.datomic/datomic-pro "0.9.5661"
+                 [com.datomic/datomic-pro "0.9.5703"
                   :exclusions [org.slf4j/slf4j-nop
                                org.slf4j/slf4j-log4j12]]
-                 [com.amazonaws/aws-java-sdk-dynamodb "1.11.261"
+                 [com.amazonaws/aws-java-sdk-dynamodb "1.11.355"
                   :exclusions [commons-codec commons-logging]]
-                 [ch.qos.logback/logback-classic "1.2.2"]
-                 [democracyworks/kehaar "0.11.3"]]
+                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [democracyworks/kehaar "0.11.4"]]
   :plugins [[com.pupeno/jar-copier "0.4.0"]]
-  :java-agents [[com.newrelic.agent.java/newrelic-agent "4.1.0"]]
+  :java-agents [[com.newrelic.agent.java/newrelic-agent "4.2.0"]]
   :jar-copier {:java-agents true
                :destination "resources/jars"}
   :prep-tasks ["javac" "compile" "jar-copier"]
