18b3d6e4ccfb76b6602dec61f3986a7eb0b0e576
Wes Morgan <wesmorgan@icloud.com>
2018-12-04 11:46:40 -0700

    Add core.async dep for kehaar 1.0.0 compatibility

--- resources/leiningen/new/kraken_works/project.clj
+++ resources/leiningen/new/kraken_works/project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/tools.logging "0.4.1"]
+                 [org.clojure/core.async "0.4.474"]
                  [democracyworks/resource-config "1.0.0"]
 {{#datomic?}}
                  [democracyworks/datomic-toolbox "2.0.5"
