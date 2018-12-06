21e6d20bec7f1042395c7f86de4bc01a15cf883d
Wes Morgan <wesmorgan@icloud.com>
2018-12-06 11:56:17 -0700

    Bump version to 1.0.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kraken-works/lein-template "0.6.1-SNAPSHOT"
+(defproject kraken-works/lein-template "1.0.0-SNAPSHOT"
   :description "Leiningen template for kraken-works components"
   :url "https://github.com/democracyworks/kraken-works-lein-template"
   :license {:name "Eclipse Public License"
