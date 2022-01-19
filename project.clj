cf193dbb5d3b4b7325c6c9a1bd4abb2e20cf3d2c
Gordon Stratton <gordon@democracy.works>
2022-01-18 22:19:12 -0800

    Version 1.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kraken-works/lein-template "1.1.2-SNAPSHOT"
+(defproject kraken-works/lein-template "1.2.0"
   :description "Leiningen template for kraken-works components"
   :url "https://github.com/democracyworks/kraken-works-lein-template"
   :license {:name "Eclipse Public License"
